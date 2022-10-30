4b4306547a3dc328cd44c4a79632320d92c3f2f8
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-30 20:17:41 +0100

    add navigation lib

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 
 export default function App() {
 	return (
 		<TailwindProvider>
-			<View className="flex-1 justify-center items-center">
-				<Text>Hello From My app to your Iphone</Text>
-				<StatusBar style="auto" />
+			<View className="py-10 px-3 ">
+				<Text>Hello !</Text>
 			</View>
 		</TailwindProvider>
 	);

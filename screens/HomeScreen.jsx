21d7f50337999ac635f0369baa8dc817a50621f9
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-30 23:27:40 +0100

    add Speech method

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,21 @@
 import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import * as Speech from "expo-speech";
 
 function HomeScreen() {
+	Speech.speak("Hello there ! What you want to do today ?");
+
 	return (
 		<TailwindProvider>
-			<View className="flex-1 items-center justify-center">
-				<Text>Hello from home screen</Text>
+			<View className="">
+				<Text
+					className="p-3 text-2xl my-5 text-center text-gray-700"
+					onPress={() =>
+						Speech.speak("Hello there ! What you want to do today ?")
+					}
+				>
+					Hello there ! What you want to do today ?
+				</Text>
 			</View>
 		</TailwindProvider>
 	);
